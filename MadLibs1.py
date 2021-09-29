47b3c74c72f0875ccba265d1e169e96e0106e809
Fusilladin <91526142+Fusilladin@users.noreply.github.com>
2021-09-29 18:48:16 -0500

    Add files via upload

---
+++ MadLibs1.py
@@ -0,0 +1,12 @@
+Python Mad Libs 1
+
+----
+
+animal_one=input("Animal   ")
+verb_two=input("Verb   ")
+verb_one=input("Verb   ")
+vehicle_one=input("Vehicle   ")
+
+print("Driving down the road you see a massive", animal_one, "blocking your way.")
+print("Your", vehicle_one, "comes to a screeching halt as you almost run it over going full speed.")
+print("You", verb_one, "out of the", vehicle_one, "and the", animal_one, verb_two, "towards you.")
